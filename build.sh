821e64de99d04ef9c5be095adb7d0295782f86b3
Sergey Slotin <me@sereja.me>
2019-06-05 00:21:18 +0300

    getting it right

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-for file in `ls articles | grep .md`
-do
-    pandoc file -o segtree.html --css pandoc.css --standalone --mathjax
-done
+cp -r img web/img
+mkdir web/{ru,en}
+find {ru,en} -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "web/${0%.md}.html" --css /pandoc.css --standalone --mathjax' {} \;
