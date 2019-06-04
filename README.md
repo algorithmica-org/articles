adb51c5757ff0c96e12875825102f9b8cc9715dd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-04 22:12:48 +0000

    Deploy algorithmica-org/articles to github.com/algorithmica-org/articles.git:gh-pages

--- README.md
+++
@@ -1,3 +0,0 @@
-# Algorithmica.org
-
-test
--- build.sh
+++
@@ -1,3 +0,0 @@
-cp -r img web/img
-mkdir web/{ru,en}
-find {ru,en} -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "web/${0%.md}.html" --css ../pandoc.css --standalone --mathjax' {} \;
