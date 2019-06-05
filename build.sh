2d96bace9cb012ea9ef40a69ceadaef7ad2cc79c
Sergey Slotin <me@sereja.me>
2019-06-05 13:43:14 +0300

    restructuring

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-cp -r img web/img
-mkdir web/{ru,en}
-find {ru,en} -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "web/${0%.md}.html" --css ../pandoc.css --standalone --mathjax' {} \;
+#!/bin/bash
+cp -r src/img web/
+mkdir -p web/{ru,en}
+#find src/{ru,en} -iname "*.md" -type f -exec sh -c 'echo "${0#src/}"' {} \;
+cd src 
+find {ru,en} -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "../web/${0%.md}.html" --css ../pandoc.css --standalone --mathjax' {} \;
