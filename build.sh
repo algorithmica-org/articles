bb8663eea51150f8ae868b10a536c9aed605875a
Sergey Slotin <me@sereja.me>
2019-06-05 16:42:49 +0300

    clean-up

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cp -r src/img web/
 mkdir -p web/{ru,en}
-#find src/{ru,en} -iname "*.md" -type f -exec sh -c 'echo "${0#src/}"' {} \;
 cd src 
 find {ru,en} -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "../web/${0%.md}.html" --css ../pandoc.css --standalone --mathjax' {} \;
