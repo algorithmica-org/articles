0502e581443d1ef6e3a0b2b9e714605c571301c4
Sergey Slotin <me@sereja.me>
2019-06-06 21:00:13 +0300

    fixing relative path

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 cp -r src/img web/
 mkdir -p web/{ru,en}
 cd src 
-find {ru,en} -iname "*.md" -type f -exec sh -c 'pandoc "${0}" web/metrica.html -o "../web/${0%.md}.html" --css ../pandoc.css --standalone --mathjax' {} \;
+find {ru,en} -iname "*.md" -type f -exec sh -c 'pandoc "${0}" ../web/metrica.html -o "../web/${0%.md}.html" --css ../pandoc.css --standalone --mathjax' {} \;
