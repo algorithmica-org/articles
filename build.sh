458dd7e3f94a4cceba336d5f6b00f0ef40329a20
Sergey Slotin <me@sereja.me>
2019-06-08 17:12:06 +0300

    titles

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 cp -r src/img web/
 mkdir -p web/{ru,en}
 cd src
-find {ru,en} -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "../web/${0%.md}.html" -H ../web/head.html --css ../pandoc.css --standalone --mathjax' {} \;
+find {ru,en} -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "../web/${0%.md}.html" -H ../web/head.html --css ../pandoc.css --metadata title="`grep -m1 -Po "^# *\K(.*)$" ${0}`" --standalone --mathjax' {} \;
