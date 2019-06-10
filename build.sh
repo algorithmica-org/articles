8f87347c14efb58b989c55dc39dd5fbd17fe65ae
Sergey Slotin <me@sereja.me>
2019-06-10 14:43:45 +0300

    refactoring build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -x
 cp -r src/img web/
 mkdir -p web/{ru,en}
 cd src
-find {ru,en} -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "../web/${0%.md}.html" -H ../web/head.html --css ../pandoc.css --metadata title="`grep -m1 -Po "^# *\K(.*)$" ${0}`" --standalone --mathjax' {} \;
+for file in `find {ru,en} -iname "*.md" -type f`
+do
+    pandoc "${file}" \
+    -o "../web/${file%.md}.html" \
+    -H ../web/head.html \
+    --css ../pandoc.css \
+    --metadata title="`grep -m1 -Po "^# *\K(.*)$" ${file}`" \
+    --standalone --mathjax
+done
