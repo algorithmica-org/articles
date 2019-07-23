046ab8bb3d39e456e83ba24176fb4b5fe0b1612b
Sergey Slotin <me@sereja.me>
2019-07-24 02:35:07 +0300

    new html template

--- build.sh
+++ build.sh
@@ -4,10 +4,20 @@ mkdir -p web/{ru,en}
 cd src
 for file in `find {ru,en} -iname "*.md" -type f`
 do
+    basename=`basename ${file%.md}`
+    index="FALSE"  # TODO: swap true and false
+    title=`grep -m1 -Po "^# *\K(.*)$" ${file}`
+    if [ "$basename" != "index" ]; then
+        title="${title} - Алгоритмика"
+        index="TRUE"
+    fi
     pandoc "${file}" \
     -o "../web/${file%.md}.html" \
     -H ../web/head.html \
     --css ../pandoc.css \
-    --metadata title="`grep -m1 -Po "^# *\K(.*)$" ${file}`" \
+    --template ../web/template.html \
+    --metadata title="$title" \
+    --variable filename=$basename \
+    --metadata index=$index \
     --standalone --mathjax
 done
