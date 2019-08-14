b30369987d723314a2d24ca9807381b1421a8698
Sergey Slotin <me@sereja.me>
2019-08-14 04:48:35 +0300

    localization v2

--- build.sh
+++ build.sh
@@ -5,18 +5,23 @@ cd src
 for file in `find {en,ru,tg} -iname "*.md" -type f`
 do
     section=${file:0:2}
+    edit="Редактировать"
+    view_history="История"
     if [ "$section" = "ru" ]; then
         sitename="Алгоритмика"
     fi
     if [ "$section" = "en" ]; then
         sitename="Algorithmica"
+        edit="Edit this page"
+        view_history="View history"
     fi
     if [ "$section" = "tg" ]; then
         sitename="Tinkoff Generation"
     fi
-    basename=`dirname ${file%.md}`
+    basename=`basename ${file%.md}`
     index="FALSE"  # TODO: swap true and false
     title=`grep -m1 -Po "^# *\K(.*)$" ${file}`
+    echo "Building ${file}..."
     if [ "$basename" != "index" ]; then
         title="$title - $sitename"
         index="TRUE"
@@ -30,6 +35,8 @@ do
     --variable filename="$basename" \
     --variable section="$section" \
     --variable sitename="$sitename" \
+    --variable edit="$edit" \
+    --variable view_history="$view_history" \
     --metadata index="$index" \
     --standalone --mathjax
 done
