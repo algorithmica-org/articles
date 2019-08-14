b9868838d35a0b3239a16b57c5c0908fdb3c13b7
Sergey Slotin <me@sereja.me>
2019-08-14 03:31:47 +0300

    localization v1

--- build.sh
+++ build.sh
@@ -4,11 +4,21 @@ mkdir -p web/{en,ru,tg}
 cd src
 for file in `find {en,ru,tg} -iname "*.md" -type f`
 do
-    basename=`basename ${file%.md}`
+    section=${file:0:2}
+    if [ "$section" = "ru" ]; then
+        sitename="Алгоритмика"
+    fi
+    if [ "$section" = "en" ]; then
+        sitename="Algorithmica"
+    fi
+    if [ "$section" = "tg" ]; then
+        sitename="Tinkoff Generation"
+    fi
+    basename=`dirname ${file%.md}`
     index="FALSE"  # TODO: swap true and false
     title=`grep -m1 -Po "^# *\K(.*)$" ${file}`
     if [ "$basename" != "index" ]; then
-        title="${title} - Алгоритмика"
+        title="$title - $sitename"
         index="TRUE"
     fi
     pandoc "${file}" \
@@ -17,7 +27,9 @@ do
     --css ../pandoc.css \
     --template ../web/template.html \
     --metadata title="$title" \
-    --variable filename=$basename \
-    --metadata index=$index \
+    --variable filename="$basename" \
+    --variable section="$section" \
+    --variable sitename="$sitename" \
+    --metadata index="$index" \
     --standalone --mathjax
 done
